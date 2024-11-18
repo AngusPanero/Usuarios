39f5e429a163eed9755b9f32a65dd0e883aafda7
Agustin Panero <agustinpanero@MacBook-Pro-de-Agustin.local>
2024-11-18 23:48:56 +0100

    JS Terminado Desestructurado

--- js/script.js
+++ js/script.js
@@ -14,23 +14,29 @@ function usuersFetch () {
     })
     .then((data) => {
         console.log(data);
-        data.forEach((user) => {
+
+        data.forEach(({id, name, username, email, address, phone, company}) => {//en vez de data en el for each porngo mi obj desestructurado
+
         const crearLi = document.createElement("li");
-        const imgUser = `/assets/img/${user.id}.jpeg`;
+        const imgUser = `/assets/img/${id}.jpeg`;
+        const numeroAle = Math.floor(Math.random() * (40 - 25 + 1)) + 25;// el valor del primer () es 16 el random me hace 16 posiciones aleatorias, mas 25 llego al maximo de 40 que busco
+        const {street, suite, city} = address;
+        const {name: nombreComapñia} = company // le doy un nombre random al {name: ""}
+
         crearLi.innerHTML = `
         <img src="${imgUser}">
         <div class="datosPersonales">
-            <p>Nombre: ${user.name}</p>
-            <p>Edad: 28</p>
-            <p>Username: ${user.username}</p>
-            <p>Telefono: ${user.phone}</p>
-            <p>Email: ${user.email}</p>
+            <p>Nombre: ${name}</p>
+            <p>Edad: ${numeroAle}</p>
+            <p>Username: ${username}</p>
+            <p>Telefono: ${phone}</p>
+            <p>Email: ${email}</p>
         </div>
         <div class="compañia">
-            <p>Compañia: ${user.company}</p>
-            <p>Direccion: ${user.address.steet}, ${user.address.suite}, ${user.address.city}</p>
+            <p>Compañia: ${company.name}</p>
+            <p>Direccion: ${street}, ${suite}, ${city}</p>
         </div>
-        `
+        `// aca ${voy directo al valor} por la desestructuracion no hace falta poner user.email pongo directo ${email}        
         obtenerUl.appendChild(crearLi);
         });
     })
